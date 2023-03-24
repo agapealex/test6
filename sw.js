db400b785041541ccb013aa5a1c98a6fdf74101e
agapealex <agapee.alex@yahoo.com>
2023-03-24 03:51:38 +0200

    Updates

--- sw.js
+++ sw.js
@@ -126,9 +126,10 @@ self.addEventListener('fetch', function(event) {
       .then(function(response) {
         if (response) {
           return response;
-        } else {
-          return fetch(event.request);
-        }
+        } 
+        // else {
+        //   return fetch(event.request);
+        // }
         // else {
         //   return fetch(event.request)
         //     .then(function(res) {
