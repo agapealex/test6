9d4b863f5f733e6c7f2fcd11b6e83e6e52493401
agapealex <agapee.alex@yahoo.com>
2023-03-30 10:49:58 +0300

    Updates

---
+++ precache-manifest.c2b8d3f01b9c8748888209597813e342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "782b65eaaf9118835604",
+    "url": "$%7BREACT_APP_HOMEPAGE%7D/static/css/main.f760a7ea.chunk.css"
+  },
+  {
+    "revision": "782b65eaaf9118835604",
+    "url": "$%7BREACT_APP_HOMEPAGE%7D/static/js/main.7dc71679.chunk.js"
+  },
+  {
+    "revision": "2ade2f14f13c2e836c27",
+    "url": "$%7BREACT_APP_HOMEPAGE%7D/static/js/runtime~main.e0116690.js"
+  },
+  {
+    "revision": "5ca08a84fb8356bbfe7c",
+    "url": "$%7BREACT_APP_HOMEPAGE%7D/static/js/2.f39619f7.chunk.js"
+  },
+  {
+    "revision": "1050e875097810d63e58be7a37d79605",
+    "url": "$%7BREACT_APP_HOMEPAGE%7D/index.html"
+  }
+];
