339fa2318ed99b2e56bae65f54ef40052cd4f98e
agapealex <agapee.alex@yahoo.com>
2023-03-31 16:21:46 +0300

    dgdf

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ import { Provider } from 'react-redux';
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/rootReducer";
 
+// if (process.env.NODE_ENV === 'production') {
+//   import('../public/manifest.prod.json').then(manifest => {
+//     const link = document.createElement('link');
+//     link.setAttribute('rel', 'manifest');
+//     link.setAttribute('href', manifest.default);
+//     document.head.appendChild(link);
+//   });
+// } else {
+//   const link = document.createElement('link');
+//   link.setAttribute('rel', 'manifest');
+//   link.setAttribute('href', '/manifest.json');
+//   document.head.appendChild(link);
+// }
+
 
 const store = configureStore({
   reducer: rootReducer,
